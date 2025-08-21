5c0d7622c3904614aedfe0419af1faf118aa8f86
ViniStars <viniciusm9365@gmail.com>
2025-08-20 21:21:38 -0300

    Antes da automatização

--- forca_FINAL/MainForm.Designer.cs
+++ forca_FINAL/MainForm.Designer.cs
@@ -46,6 +46,7 @@ namespace forca_FINAL
 		private System.Windows.Forms.Panel panel1;
 		private System.Windows.Forms.PictureBox pictureBox1;
 		private System.Windows.Forms.Button Ç;
+		private System.Windows.Forms.Button button2;
 
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -68,6 +69,7 @@ namespace forca_FINAL
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
 			this.button1 = new System.Windows.Forms.Button();
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.label1 = new System.Windows.Forms.Label();
@@ -100,6 +102,7 @@ namespace forca_FINAL
 			this.panel1 = new System.Windows.Forms.Panel();
 			this.Ç = new System.Windows.Forms.Button();
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button2 = new System.Windows.Forms.Button();
 			this.panel1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
@@ -125,12 +128,11 @@ namespace forca_FINAL
 			// 
 			// label1
 			// 
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(427, 110);
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(449, 110);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(369, 96);
+			this.label1.Size = new System.Drawing.Size(596, 96);
 			this.label1.TabIndex = 2;
-			this.label1.Text = "label1";
 			this.label1.Visible = false;
 			// 
 			// A
@@ -423,9 +425,9 @@ namespace forca_FINAL
 			this.panel1.Controls.Add(this.C);
 			this.panel1.Controls.Add(this.B);
 			this.panel1.Controls.Add(this.A);
-			this.panel1.Location = new System.Drawing.Point(316, 285);
+			this.panel1.Location = new System.Drawing.Point(371, 309);
 			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(794, 268);
+			this.panel1.Size = new System.Drawing.Size(783, 239);
 			this.panel1.TabIndex = 29;
 			// 
 			// Ç
@@ -440,18 +442,31 @@ namespace forca_FINAL
 			// 
 			// pictureBox1
 			// 
-			this.pictureBox1.Location = new System.Drawing.Point(34, 178);
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(12, 70);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(237, 285);
+			this.pictureBox1.Size = new System.Drawing.Size(331, 441);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
 			this.pictureBox1.TabIndex = 30;
 			this.pictureBox1.TabStop = false;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(969, 45);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(117, 62);
+			this.button2.TabIndex = 31;
+			this.button2.Text = "Reiniciar";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.Indigo;
-			this.ClientSize = new System.Drawing.Size(1136, 664);
+			this.ClientSize = new System.Drawing.Size(1166, 664);
+			this.Controls.Add(this.button2);
 			this.Controls.Add(this.pictureBox1);
 			this.Controls.Add(this.panel1);
 			this.Controls.Add(this.label1);
